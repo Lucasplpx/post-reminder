7a444c9199e6d0f350d92699151e785b333000a3
Lucas <lucasplpx@gmail.com>
2021-03-07 20:05:42 -0300

    criacao do contador

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { Notifier } from './Notifier.js';
+import { Timer } from './Timer.js';
 
 const App = {
   async start() {
     try {
+      const time = 0.1 * 60;
+      Timer.init(time);
       await Notifier.init();
       Notifier.notifi({
         title: 'Hora do Post',
